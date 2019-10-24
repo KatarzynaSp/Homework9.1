30a6872de0fc96e1ac3df8f6dc46e9b54ae7a02b
Katarzyna <katarzyna.panek111@gmail.com>
2019-10-24 21:03:10 +0200

    versja 0

--- src/Array.java
+++ src/Array.java
@@ -1,20 +1,23 @@
 import java.util.Random;
 
 public class Array {
+
+
     public static int[] createArray(int arrayLength) {
+        Random random = new Random();
         int array[] = new int[arrayLength];
         for (int i = 0; i < arrayLength; i++) {
-            Random random = new Random();
             array[i] = random.nextInt();
         }
         return array;
     }
 
     public static void showInfo(int[] array) {
-        for (int i = 0; i < 2 * array.length; i++) {
+        for (int i = 0; i < (2 * array.length); i++) {
             if (i < array.length) {
                 System.out.print(array[i] + " ");
-            } else System.out.print(array[2 * array.length - 1 - i] + " ");
+            } else
+                System.out.print(array[2 * array.length - 1 - i] + " ");
         }
     }
 }
